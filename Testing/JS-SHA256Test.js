384bb5fc92770d0a1f573e0bc891efea66d83a3d
Nael <sersynthaz3@gmail.com>
2018-03-09 21:32:16 -0500

    js-sha256 Testing file
    File used after install to test if js-sha256 is working and installed properly

---
+++ Testing/JS-SHA256Test.js
@@ -0,0 +1,13 @@
+//Node.JS module Require statement
+var sha256 = require('js-sha256');
+
+//Console Output
+console.log("Connected")
+//SHA256 array Generator using js-sha256
+sha256.array('');
+//SHA256 output
+console.log(sha256.array(""));
+/*
+By: Synthetic Atom
+3/9/18
+*/
