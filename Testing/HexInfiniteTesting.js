10e81b2736e203b985703615e34315a6c7f28ad6
Nael <sersynthaz3@gmail.com>
2018-03-11 22:07:41 -0400

    Hex Loop Testing
    Returns NaN, working on fix.

---
+++ Testing/HexInfiniteTesting.js
@@ -0,0 +1,15 @@
+var sha256 = require('js-sha256');
+
+function Hex() {
+  sha256.hex('DataGen');  //Output Pre Incrementation: 3A8DA37699EE780EA152797F38DC8F6157B65416231A0EC3420E4717BE8E3E12
+  var Alpha = "3";
+  var Beta = "2";
+var Test = Alpha + Beta;
+}
+DataGen();
+
+while(Test = 5) {
+  console.log(sha256.hex)
+  sha256.hex++
+  //Returns NaN, this is not useful.*Will be fixed soon
+}
